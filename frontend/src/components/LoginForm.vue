8e67bbdbb6c51e2523efc70457b8ceb4920edb6c
Olivier Basquin <olivier.basquin.iphone@gmail.com>
2021-08-09 20:59:04 +0200

    maj

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper">
+        <img src="/assets/img/groupomania-logo.png" alt="Groupomania logo">
+        <nav><router-link to="/" class="active">Se connecter</router-link> | <router-link to="/signup">S'inscrire</router-link></nav>
+        <form>
+
+            <label for="login-email">Email :</label>
+            <input id="login-email" type="text" placeholder="Email" required>
+            
+            <label for="login-password">Mot de passe :</label>
+            <input id="login-password" type="password" placeholder="Mot de passe" required>
+
+            <div class="error-message">{{message}}</div>
+
+            <button id="login-btn" type="submit">Connexion</button>
+
+            
+        </form>
+    </div>
+</template>
